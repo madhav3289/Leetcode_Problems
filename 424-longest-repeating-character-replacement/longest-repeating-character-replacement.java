3abfe6a1f10abba919d7d592da1433debe812459
Madhav Gupta <madhavguptadavfps1234@gmail.com>
2025-10-08 01:28:00 +0530

    Time: 38 ms (13.55%) | Memory: 44.6 MB (84.20%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
@@ -2,25 +2,27 @@ class Solution {
     public int characterReplacement(String s, int k) {
         int max=0;
         for(int i=0;i<26;i++){
-            max=Math.max(max,maxConsecutive(s,i,k));
+            max=Math.max(max,longestReplacement(s,k,i));
         }
         return max;
     }
-    public static int maxConsecutive(String s,int i,int k){
-        char sp=(char)(i+'A');
-        int max=0;
+    public static int longestReplacement(String s,int k,int i){
+        char ch=(char)(i+'A');
         int lo=0,hi=0;
+        int max=0;
         while(hi<s.length()){
-            char ch=s.charAt(hi);
-            if(sp!=ch){
+            char temp=s.charAt(hi);
+            if(temp!=ch){
                 k--;
                 while(k<0){
-                    if(s.charAt(lo)!=sp){
+                    char x=s.charAt(lo);
+                    if(x!=ch){
                         k++;
                     }
                     lo++;
                 }
             }
+            
             max=Math.max(max,hi-lo+1);
             hi++;
         }

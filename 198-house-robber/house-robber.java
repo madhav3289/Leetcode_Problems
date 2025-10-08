3acce8d2047745f98f9d4916366ad8842f3f6cc9
Madhav Gupta <madhavguptadavfps1234@gmail.com>
2025-10-08 10:29:10 +0530

    Time: 0 ms (100.00%) | Memory: 41 MB (78.35%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -1,21 +1,19 @@
 class Solution {
     public int rob(int[] nums) {
-        int n=nums.length;
-        int [] memo=new int[n];
-        Arrays.fill(memo,-1);
-        return helper(nums,0,memo);
+        int [] dp=new int[nums.length];
+        Arrays.fill(dp,-1);
+        return helper(nums,0,dp);        
     }
-    public static int helper(int [] nums,int idx,int [] memo){
+    public static int helper(int [] nums,int idx,int [] dp){
         if(idx>=nums.length){
             return 0;
         }
-        if(memo[idx]!=-1){
-            return memo[idx];
+        if(dp[idx]!=-1){
+            return dp[idx];
         }
-        // we can either rob one house and then skip one and move to another
-        int inc=nums[idx]+helper(nums,idx+2,memo);
-        // or we can skip current house and move to next house
-        int exc=helper(nums,idx+1,memo);
-        return memo[idx]=Math.max(inc,exc);
+        int rob=nums[idx]+helper(nums,idx+2,dp);
+        int drob=helper(nums,idx+1,dp);
+
+        return dp[idx]=Math.max(rob,drob);
     }
 }

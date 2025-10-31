c374bfcd021683cdf3c3a89cfc7d1e7e875622e8
Madhav Gupta <madhavguptadavfps1234@gmail.com>
2025-10-31 18:50:39 +0530

    Time: 13 ms (24.65%) | Memory: 61.2 MB (5.70%) - LeetSync

--- 148-sort-list/sort-list.java
+++ 148-sort-list/sort-list.java
@@ -10,19 +10,53 @@
  */
 class Solution {
     public ListNode sortList(ListNode head) {
-        List<Integer> list=new ArrayList<>();
-        ListNode temp=head;
-        while(temp!=null){
-            list.add(temp.val);
+        if(head==null || head.next==null){
+            return head;
+        }
+
+        ListNode middle=middleOfList(head);
+        ListNode leftList=head;
+        ListNode rightList=middle.next;
+        middle.next=null;
+
+        leftList=sortList(leftList);
+        rightList=sortList(rightList);
+
+        return mergeTwoLists(leftList,rightList);
+    }
+    public static ListNode mergeTwoLists(ListNode head1,ListNode head2){
+        ListNode dummy=new ListNode(-1);
+        ListNode temp=dummy;
+        while(head1!=null && head2!=null){
+            if(head1.val<=head2.val){
+                temp.next=new ListNode(head1.val);
+                head1=head1.next;
+            }
+            else{
+                temp.next=new ListNode(head2.val);
+                head2=head2.next;
+            }
             temp=temp.next;
         }
-        Collections.sort(list);
-        temp=head;
-        int idx=0;
-        while(temp!=null){
-            temp.val=list.get(idx++);
+        while(head1!=null){
+            temp.next=new ListNode(head1.val);
+            head1=head1.next;
             temp=temp.next;
         }
-        return head;
+        while(head2!=null){
+            temp.next=new ListNode(head2.val);
+            head2=head2.next;
+            temp=temp.next;
+        }
+        return dummy.next;
+    }
+    public static ListNode middleOfList(ListNode head){
+        ListNode slow=head;
+        ListNode fast=head.next;
+        while(fast!=null && fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+        }
+        return slow;
     }
 }

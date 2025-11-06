bc4fa2510e12e5c3cb8d10bacca7433148bbd3c8
Madhav Gupta <madhavguptadavfps1234@gmail.com>
2025-11-07 01:52:13 +0530

    Time: 5 ms (70.93%) | Memory: 45.7 MB (5.13%) - LeetSync

---
+++ 273-integer-to-english-words/integer-to-english-words.java
@@ -0,0 +1,29 @@
+class Solution {
+    static String [] ones={"","One","Two","Three","Four","Five","Six","Seven","Eight","Nine"};
+    static String [] twos={"Ten","Eleven","Twelve","Thirteen","Fourteen","Fifteen","Sixteen","Seventeen","Eighteen","Nineteen"};
+    static String [] tens={"","Ten","Twenty","Thirty","Forty","Fifty","Sixty","Seventy","Eighty","Ninety"};
+    public String numberToWords(int num) {
+        if(num==0){
+            return "Zero";
+        }
+        if(num<10){
+            return ones[num];
+        }
+        if(num<20){
+            return twos[num-10];
+        }
+        if(num<100){
+            return tens[num/10]+((num%10==0)?"":" "+numberToWords(num%10));
+        }
+        if(num<1000){
+            return ones[num/100]+" Hundred"+((num%100==0)?"":" "+numberToWords(num%100));
+        }
+        if(num<1000000){
+            return numberToWords(num/1000)+" Thousand"+((num%1000==0)?"":" "+numberToWords(num%1000));
+        }
+        if(num<1000000000){
+            return numberToWords(num/1000000)+" Million"+((num%1000000==0)?"":" "+numberToWords(num%1000000));
+        }
+        return numberToWords(num/1000000000)+" Billion"+((num%1000000000==0)?"":" "+numberToWords(num%1000000000));
+    }
+}

40c16a6a0c0d55c2794ff29317e688f33395352a
Madhav Gupta <madhavguptadavfps1234@gmail.com>
2025-11-06 22:36:27 +0530

    Time: 70 ms (55.52%) | Memory: 42.8 MB (19.20%) - LeetSync

--- 1331-path-with-maximum-gold/path-with-maximum-gold.java
+++ 1331-path-with-maximum-gold/path-with-maximum-gold.java
@@ -4,35 +4,31 @@ class Solution {
     public int getMaximumGold(int[][] grid) {
         n=grid.length;
         m=grid[0].length;
-
-        boolean [][] isVisited=new boolean[n][m];
-
+        boolean [][] visited=new boolean[n][m];
         int max=0;
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(grid[i][j]==0){
-                    continue;
-                }
-                else{
-                    max=Math.max(max,helper(grid,i,j,isVisited));
+                if(grid[i][j]!=0){
+                    max=Math.max(max,helper(grid,visited,i,j));
                 }
             }
         }
         return max;
     }
-    public static int helper(int [][] grid,int i,int j,boolean [][] isVisited){
-        if(i<0 || j<0 || i>=n || j>=m){
+    public static int helper(int [][] grid,boolean [][] visited,int row,int col){
+        if(row<0 || col<0 || row>=n || col>=m){
             return 0;
         }
-        if(grid[i][j]==0 || isVisited[i][j]){
+        if(grid[row][col]==0 || visited[row][col]){
             return 0;
         }
-        isVisited[i][j]=true;
-        int left=helper(grid,i,j-1,isVisited);
-        int up=helper(grid,i-1,j,isVisited);
-        int right=helper(grid,i,j+1,isVisited);
-        int down=helper(grid,i+1,j,isVisited);
-        isVisited[i][j]=false;
-        return grid[i][j]+Math.max(Math.max(up,down),Math.max(right,left));
+        visited[row][col]=true;
+        int up=helper(grid,visited,row-1,col);
+        int left=helper(grid,visited,row,col-1);
+        int down=helper(grid,visited,row+1,col);
+        int right=helper(grid,visited,row,col+1);
+        visited[row][col]=false;
+
+        return Math.max(Math.max(up,down),Math.max(right,left))+grid[row][col];
     }
 }

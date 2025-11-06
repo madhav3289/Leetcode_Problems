7b6b0c732f494b9facca941aec598474fe56e6a6
Madhav Gupta <madhavguptadavfps1234@gmail.com>
2025-11-06 22:46:22 +0530

    Time: 10 ms (14.31%) | Memory: 65.2 MB (5.55%) - LeetSync

--- 131-palindrome-partitioning/palindrome-partitioning.java
+++ 131-palindrome-partitioning/palindrome-partitioning.java
@@ -1,29 +1,27 @@
 class Solution {
+    static List<List<String>> res;
     public List<List<String>> partition(String s) {
-        List<List<String>> res=new ArrayList<>();
-        helper(s,res,new ArrayList<>(),0);
+        res=new ArrayList<>();
+        helper(s,new ArrayList<>(),0);
         return res;
     }
-    public static void helper(String s,List<List<String>> res,List<String> temp,int idx){
-        if(idx>s.length()){
-            return;
-        }
+    public static void helper(String s,List<String> temp,int idx){
         if(idx==s.length()){
             res.add(new ArrayList<>(temp));
             return;
         }
         for(int i=idx;i<s.length();i++){
-            String sub=s.substring(idx,i+1);
-            if(isPalindrome(sub)){
-                temp.add(sub);
-                helper(s,res,temp,i+1);
+            if(isPalindrome(s.substring(idx,i+1))){
+                temp.add(s.substring(idx,i+1));
+                helper(s,temp,i+1);
                 temp.remove(temp.size()-1);
             }
         }
     }
     public static boolean isPalindrome(String s){
-        int lo=0,hi=s.length()-1;
-        while(lo<=hi){
+        int lo=0;
+        int hi=s.length()-1;
+        while(lo<hi){
             if(s.charAt(lo)!=s.charAt(hi)){
                 return false;
             }

005a9d484d9399e5b46293186b6264b79ab3a959
Madhav Gupta <madhavguptadavfps1234@gmail.com>
2025-11-06 16:22:26 +0530

    Time: 0 ms (100.00%) | Memory: 42.9 MB (16.39%) - LeetSync

--- 1022-unique-paths-iii/unique-paths-iii.java
+++ 1022-unique-paths-iii/unique-paths-iii.java
@@ -1,54 +1,47 @@
 class Solution {
-
     static int n;
     static int m;
-
+    static int totalSteps;
     public int uniquePathsIII(int[][] grid) {
-        // we have to walk over each and every empty squares and reach a target.
-
         n=grid.length;
         m=grid[0].length;
-        boolean [][] isVisited=new boolean[n][m];
-
-        int t_steps=0;
+        totalSteps=0;
+        int row=0,col=0;
+        boolean [][] visited=new boolean[n][m];
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(grid[i][j]==0){
-                    t_steps++;
+                    totalSteps++;
+                }
+                else if(grid[i][j]==1){
+                    row=i;
+                    col=j;
                 }
             }
         }
-
-        int res=0;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(grid[i][j]==1){
-                    res+=helper(grid,isVisited,t_steps+1,0,i,j);
-                }
-            }
-        }
-        return res;
+        return helper(grid,row,col,visited,-1);
+        
     }
-    public static int helper(int [][] grid,boolean [][] isVisited,int t_steps,int c_steps,int i,int j){
-        if(i<0 || j<0 || i>=n || j>=m){
+    public static int helper(int [][] grid,int row,int col,boolean [][] isVisited,int cSteps){
+        if(row<0 || col<0 || row>=n || col>=m){
             return 0;
         }
-        if(grid[i][j]==-1 || isVisited[i][j]){
+        if(grid[row][col]==-1 || isVisited[row][col]){
             return 0;
         }
-        if(grid[i][j]==2){
-            if(t_steps==c_steps){
+        if(grid[row][col]==2){
+            if(cSteps==totalSteps){
                 return 1;
             }
             return 0;
         }
         int count=0;
-        isVisited[i][j]=true;
-        count+=helper(grid,isVisited,t_steps,c_steps+1,i,j-1);        
-        count+=helper(grid,isVisited,t_steps,c_steps+1,i-1,j);        
-        count+=helper(grid,isVisited,t_steps,c_steps+1,i,j+1);        
-        count+=helper(grid,isVisited,t_steps,c_steps+1,i+1,j);
-        isVisited[i][j]=false;
+        isVisited[row][col]=true;
+        count+=helper(grid,row,col-1,isVisited,cSteps+1);
+        count+=helper(grid,row-1,col,isVisited,cSteps+1);
+        count+=helper(grid,row,col+1,isVisited,cSteps+1);
+        count+=helper(grid,row+1,col,isVisited,cSteps+1);
+        isVisited[row][col]=false;
         return count;
     }
 }

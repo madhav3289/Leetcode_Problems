959b16002c60354e22655be07076f5cc3f7adfeb
Madhav Gupta <madhavguptadavfps1234@gmail.com>
2025-11-06 22:20:24 +0530

    Time: 136 ms (65.06%) | Memory: 43.2 MB (22.54%) - LeetSync

--- 79-word-search/word-search.java
+++ 79-word-search/word-search.java
@@ -1,12 +1,14 @@
 class Solution {
+    static int n;
+    static int m;
     public boolean exist(char[][] board, String word) {
-        int n=board.length;
-        int m=board[0].length;
-        boolean [][] isUsed=new boolean[n][m];
+        n=board.length;
+        m=board[0].length;
+        boolean [][] visited=new boolean[n][m];
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(board[i][j]==word.charAt(0)){
-                    if(helper(board,word,i,j,isUsed,0)){
+                if(word.charAt(0)==board[i][j]){
+                    if(helper(board,word,visited,i,j,0)){
                         return true;
                     }
                 }
@@ -14,29 +16,25 @@ class Solution {
         }
         return false;
     }
-    public static boolean helper(char [][] board,String word,int i,int j,boolean [][] isUsed,int idx){
-        
+    public static boolean helper(char [][] board,String word,boolean [][] visited,int row,int col,int idx){
         if(idx==word.length()){
             return true;
         }
-        if(i<0 || j<0 || i>=board.length || j>=board[0].length){
+        if(row<0 || col<0 || row>=n || col>=m){
             return false;
         }
-        if(word.charAt(idx)!=board[i][j]){
+        if(word.charAt(idx)!=board[row][col] || visited[row][col]){
             return false;
         }
-        if(isUsed[i][j]){
-            return false;
-        }
-        isUsed[i][j]=true;
-        boolean a=helper(board,word,i-1,j,isUsed,idx+1);
-        boolean b=helper(board,word,i,j-1,isUsed,idx+1);
-        boolean c=helper(board,word,i+1,j,isUsed,idx+1);
-        boolean d=helper(board,word,i,j+1,isUsed,idx+1);
-        if(a || b || c || d){
+        visited[row][col]=true;
+        boolean up=helper(board,word,visited,row-1,col,idx+1);
+        boolean left=helper(board,word,visited,row,col-1,idx+1);
+        boolean down=helper(board,word,visited,row+1,col,idx+1);
+        boolean right=helper(board,word,visited,row,col+1,idx+1);
+        if(up || left || down || right){
             return true;
         }
-        isUsed[i][j]=false;
+        visited[row][col]=false;
         return false;
     }
 }

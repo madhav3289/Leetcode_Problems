0da74256eaaedcc4f65cc29bd7ef61bf2900abb4
Madhav Gupta <madhavguptadavfps1234@gmail.com>
2025-11-09 15:58:30 +0530

    Time: 0 ms (100.00%) | Memory: 42.3 MB (6.69%) - LeetSync

---
+++ 1033-broken-calculator/broken-calculator.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int brokenCalc(int startValue, int target) {
+        int steps=0;
+        while(target>startValue){
+            if(target%2==0){
+                target=target/2;
+            }
+            else{
+                target++;
+            }
+            steps++;
+        }
+        if(startValue>target){
+            steps+=(startValue-target);
+        }
+        return steps;
+    }
+}

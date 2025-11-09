af8be7b955cc375ffb63b1ffde05fedb37f74e92
Madhav Gupta <madhavguptadavfps1234@gmail.com>
2025-11-09 16:23:11 +0530

    Time: 25 ms (17.83%) | Memory: 70.5 MB (5.04%) - LeetSync

--- 650-2-keys-keyboard/2-keys-keyboard.java
+++ 650-2-keys-keyboard/2-keys-keyboard.java
@@ -3,17 +3,24 @@ class Solution {
         if(n==1){
             return 0;
         }
-        return helper(n,1,1);
+        int [][] dp=new int[n+1][n+1];
+        for(int [] a:dp){
+            Arrays.fill(a,-1);
+        }
+        return helper(n,1,1,dp);
     }
-    public static int helper(int n,int curLen,int lastLen){
+    public static int helper(int n,int curLen,int lastLen,int [][] dp){
         if(curLen==n){
             return 1;
         }
         if(curLen>n){
             return 999999;
         }
-        int copyPaste=2+helper(n,curLen*2,curLen);
-        int paste=1+helper(n,curLen+lastLen,lastLen);
-        return Math.min(copyPaste,paste);
+        if(dp[curLen][lastLen]!=-1){
+            return dp[curLen][lastLen];
+        }
+        int copyPaste=2+helper(n,curLen*2,curLen,dp);
+        int paste=1+helper(n,curLen+lastLen,lastLen,dp);
+        return dp[curLen][lastLen]=Math.min(copyPaste,paste);
     }
 }

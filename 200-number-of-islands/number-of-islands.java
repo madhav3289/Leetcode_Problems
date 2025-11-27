c65fda4363a65572e368a07aa1a62e0e4247521d
Madhav Gupta <madhavguptadavfps1234@gmail.com>
2025-11-27 11:26:32 +0530

    Time: 3 ms (85.62%) | Memory: 52.3 MB (34.57%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -1,35 +1,31 @@
 class Solution {
-    public int numIslands(char[][] grid) {
-        int n=grid.length;
-        int m=grid[0].length;
+    static boolean [][] visited;
+    public int numIslands(char[][] maze) {
+        int n=maze.length;
+        int m=maze[0].length;
+        visited=new boolean[n][m];
         int count=0;
-        Set<String> set=new HashSet<>();
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(grid[i][j]=='1' && !set.contains(i+" "+j)){
+                if(maze[i][j]=='1' && !visited[i][j]){
+                    DFS(maze,i,j);
                     count++;
-                    helper(grid,set,i,j);
                 }
             }
         }
         return count;
     }
-    public static void helper(char [][] grid,Set<String> set,int i,int j){
-        if(i<0 || i>=grid.length || j<0 || j>=grid[0].length){
+    public static void DFS(char [][] maze,int r,int c){
+        if(r<0 || c<0 || r>=maze.length || c>=maze[0].length){
             return;
         }
-        if(grid[i][j]=='0'){
+        if(visited[r][c] || maze[r][c]=='0'){
             return;
         }
-        if(set.contains(i+" "+j)){
-            return;
-        }
-        else{
-            set.add(i+" "+j);
-            helper(grid,set,i-1,j);
-            helper(grid,set,i,j-1);
-            helper(grid,set,i+1,j);
-            helper(grid,set,i,j+1);
-        }
+        visited[r][c]=true;
+        DFS(maze,r-1,c);
+        DFS(maze,r,c-1);
+        DFS(maze,r+1,c);
+        DFS(maze,r,c+1);
     }
 }

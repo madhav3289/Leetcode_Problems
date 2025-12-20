fda8bdee5879eacd6bb71557ca27e2948ef3d31b
Madhav Gupta <madhavguptadavfps1234@gmail.com>
2025-12-21 00:09:59 +0530

    Time: 10 ms (7.21%) | Memory: 46.7 MB (7.10%) - LeetSync

--- 583-delete-operation-for-two-strings/delete-operation-for-two-strings.java
+++ 583-delete-operation-for-two-strings/delete-operation-for-two-strings.java
@@ -1,14 +1,31 @@
 class Solution {
     public int minDistance(String word1, String word2) {
-        int dp[][] = new int[word1.length()+1][word2.length()+1];
-        for(int i = 0; i <= word1.length(); i++) {
-            for(int j = 0; j <= word2.length(); j++) {
-                if(i == 0 || j == 0) dp[i][j] = 0;
-                else dp[i][j] = (word1.charAt(i-1) == word2.charAt(j-1)) ? dp[i-1][j-1] + 1
-                        : Math.max(dp[i-1][j], dp[i][j-1]);
-            }
+        int n=word1.length();
+        int m=word2.length();
+        int [][] dp=new int[n][m];
+        for(int [] arr:dp){
+            Arrays.fill(arr,-1);
         }
-        int val =  dp[word1.length()][word2.length()];
-        return word1.length() - val + word2.length() - val;
+        return helper(word1,word2,0,0,dp);
+    }
+    public static int helper(String s1,String s2,int i,int j,int [][] dp){
+        if(i==s1.length()){
+            return s2.length()-j;
+        }
+        if(j==s2.length()){
+            return s1.length()-i;
+        }
+        if(dp[i][j]!=-1){
+            return dp[i][j];
+        }
+        int a=0,b=0;
+        if(s1.charAt(i)==s2.charAt(j)){
+            return helper(s1,s2,i+1,j+1,dp);
+        }
+        else{
+            a=1+helper(s1,s2,i+1,j,dp);
+            b=1+helper(s1,s2,i,j+1,dp);
+        }
+        return dp[i][j]=Math.min(a,b);
     }
 }

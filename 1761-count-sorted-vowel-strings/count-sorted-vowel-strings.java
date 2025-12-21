fd8b82d11b927b4c1f6e7d79f168648d7b90ee3a
Madhav Gupta <madhavguptadavfps1234@gmail.com>
2025-12-21 14:40:44 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (13.61%) - LeetSync

--- 1761-count-sorted-vowel-strings/count-sorted-vowel-strings.java
+++ 1761-count-sorted-vowel-strings/count-sorted-vowel-strings.java
@@ -1,19 +1,37 @@
 class Solution {
     public int countVowelStrings(int n) {
-        count=0;
-        helper(n,0);
-        return count;
-    }
-    static int count=0;
-    public static void helper(int n,int idx){
-        if(n==0){
-            count++;
-            return;
+        int [][] dp=new int[n][5];
+        Arrays.fill(dp[0],1);
+        for(int i=1;i<n;i++){
+            dp[i][0]=dp[i-1][0]+dp[i-1][1]+dp[i-1][2]+dp[i-1][3]+dp[i-1][4];
+            dp[i][1]=dp[i-1][1]+dp[i-1][2]+dp[i-1][3]+dp[i-1][4];
+            dp[i][2]=dp[i-1][2]+dp[i-1][3]+dp[i-1][4];
+            dp[i][3]=dp[i-1][3]+dp[i-1][4];
+            dp[i][4]=dp[i-1][4];
         }
-        for(int i=0;i<5;i++){
-            if(i>=idx){
-                helper(n-1,i);
-            }
+        int sum=0;
+        for(int e:dp[n-1]){
+            sum+=e;
         }
+        return sum;
     }
+    // recursive code
+
+    // public int countVowelStrings(int n) {
+    //     count=0;
+    //     helper(n,0);
+    //     return count;
+    // }
+    // static int count=0;
+    // public static void helper(int n,int idx){
+    //     if(n==0){
+    //         count++;
+    //         return;
+    //     }
+    //     for(int i=0;i<5;i++){
+    //         if(i>=idx){
+    //             helper(n-1,i);
+    //         }
+    //     }
+    // }
 }

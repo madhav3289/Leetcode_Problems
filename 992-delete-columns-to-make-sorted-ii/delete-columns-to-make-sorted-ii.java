3a9dc37dfb70fae04112cc196d078d2c07033e8f
Madhav Gupta <madhavguptadavfps1234@gmail.com>
2025-12-22 11:15:16 +0530

    Time: 1 ms (77.33%) | Memory: 43.8 MB (26.67%) - LeetSync

--- 992-delete-columns-to-make-sorted-ii/delete-columns-to-make-sorted-ii.java
+++ 992-delete-columns-to-make-sorted-ii/delete-columns-to-make-sorted-ii.java
@@ -1,19 +1,25 @@
 class Solution {
-        public int minDeletionSize(String[] A) {
-        int res = 0, n = A.length, m = A[0].length(), i, j;
-        boolean[] sorted = new boolean[n - 1];
-        for (j = 0; j < m; ++j) {
-            for (i = 0; i < n - 1; ++i) {
-                if (!sorted[i] && A[i].charAt(j) > A[i + 1].charAt(j)) {
-                    res++;
+    public int minDeletionSize(String[] strs) {
+        int n=strs.length;
+        int strLen=strs[0].length();
+        int count=0;
+        boolean [] sorted=new boolean[n-1];
+        for(int i=0;i<strLen;i++){
+            boolean deleted=false;
+            for(int j=0;j<n-1;j++){
+                if(!sorted[j] && strs[j].charAt(i)>strs[j+1].charAt(i)){
+                    deleted=true;
+                    count++;
                     break;
                 }
             }
-            if (i < n - 1) continue;
-            for (i = 0; i < n - 1; ++i) {
-                sorted[i] |= A[i].charAt(j) < A[i + 1].charAt(j);
+            if(deleted){
+                continue;
+            }
+            for(int k=0;k<n-1;k++){
+                sorted[k]=sorted[k]|(strs[k].charAt(i)<strs[k+1].charAt(i));
             }
         }
-        return res;
+        return count;
     }
 }

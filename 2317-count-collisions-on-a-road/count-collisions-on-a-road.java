c58b14057665ca5f41ae6f9a697dbfab608323f0
Madhav Gupta <madhavguptadavfps1234@gmail.com>
2025-12-24 15:18:58 +0530

    Time: 99 ms (8.06%) | Memory: 48.3 MB (8.02%) - LeetSync

--- 2317-count-collisions-on-a-road/count-collisions-on-a-road.java
+++ 2317-count-collisions-on-a-road/count-collisions-on-a-road.java
@@ -1,41 +1,41 @@
 class Solution {
-    public int countCollisions(String dir) {
-        int n=dir.length();
-        Stack<Character> st=new Stack<>();
+    public int countCollisions(String s) {
+        int n=s.length();
         int count=0;
-        st.push(dir.charAt(0));
+        Stack<Character> st=new Stack<>();
+        st.push(s.charAt(0));
         for(int i=1;i<n;i++){
-            char ch=dir.charAt(i);
-            if(ch=='L'){
-                if(st.isEmpty()){
-                    st.push('L');
-                }
-                else if(st.peek()=='S'){
-                    count+=1;
-                    st.push('S');
-                }
-                else if(st.peek()=='R'){
-                    count+=2;
-                    st.pop();
-                    while(!st.isEmpty() && st.peek()=='R'){
-                        count+=1;
-                        st.pop();
-                    }
-                    st.push('S');
-                }
-                else{
-                    st.push('L');
-                }
+            char ch=s.charAt(i);
+            if(ch=='R'){
+                st.push('R');
             }
             else if(ch=='S'){
                 while(!st.isEmpty() && st.peek()=='R'){
-                    count+=1;
+                    count++;
                     st.pop();
                 }
                 st.push('S');
             }
             else{
-                st.push('R');
+                if(st.isEmpty()){
+                    st.push('L');
+                }
+                else if(st.peek()=='L'){
+                    st.push('L');
+                }
+                else if(st.peek()=='S'){
+                    count++;
+                    st.push('S');
+                }
+                else{
+                    count+=2;
+                    st.pop();
+                    while(!st.isEmpty() && st.peek()=='R'){
+                        count++;
+                        st.pop();
+                    }
+                    st.push('S');
+                }
             }
         }
         return count;

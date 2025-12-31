536042a81c216c76bef09a70973c35ec008f9340
Madhav Gupta <madhavguptadavfps1234@gmail.com>
2025-12-31 11:06:58 +0530

    Time: 11 ms (12.50%) | Memory: 47.6 MB (9.86%) - LeetSync

--- 207-course-schedule/course-schedule.java
+++ 207-course-schedule/course-schedule.java
@@ -1,51 +1,47 @@
 class Solution {
     static List<List<Integer>> list;
-    public boolean canFinish(int n, int[][] adj) {
+    public boolean canFinish(int n, int[][] edges) {
         list=new ArrayList<>();
         // create adjacency list
         for(int i=0;i<n;i++){
             list.add(new ArrayList<>());
         }
-        for(int i=0;i<adj.length;i++){
-            int a=adj[i][0];
-            int b=adj[i][1];
-            list.get(b).add(a);
+        for(int [] edge:edges){
+            list.get(edge[1]).add(edge[0]);
         }
-        // calculate the indegree of every element
-        int [] inDegree=new int[n];
+        // calculate indegree of every node
+        int [] indegree=new int[n];
         for(int i=0;i<n;i++){
             for(int j=0;j<list.get(i).size();j++){
                 int e=list.get(i).get(j);
-                inDegree[e]+=1;
+                indegree[e]++;
             }
         }
-        // topological search
-        return BFS(inDegree,n);
+        // topological sort
+        return BFS(n,indegree);
     }
-    public static boolean BFS(int [] inDegree,int n){
-        int count=0;
+    public static boolean BFS(int n,int [] indegree){
         Queue<Integer> q=new LinkedList<>();
-        // first add all elements with indegree 0
         for(int i=0;i<n;i++){
-            if(inDegree[i]==0){
-                count++;
-                q.offer(i);
+            if(indegree[i]==0){
+                q.add(i);
             }
         }
+        List<Integer> res=new ArrayList<>();
         while(!q.isEmpty()){
+            // remove
             int e=q.poll();
-            
-            for(int i=0;i<list.get(e).size();i++){
-                int temp=list.get(e).get(i);
-                // decrease indegree of all neighbours by 1 and 
-                inDegree[temp]-=1;
-                // push those to queue which have indegree 0.
-                if(inDegree[temp]==0){
-                    count++;
-                    q.offer(temp);
+            // ignore
+            // add neighbours
+            for(int nbrs:list.get(e)){
+                indegree[nbrs]--;
+                if(indegree[nbrs]==0){
+                    q.add(nbrs);
                 }
             }
+            // add to arraylist
+            res.add(e);
         }
-        return (count==n);
+        return res.size()==n;
     }
 }

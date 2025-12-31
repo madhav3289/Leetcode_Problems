f630daf96e8eb3b046d38f9e87c1bc68bb16b1bf
Madhav Gupta <madhavguptadavfps1234@gmail.com>
2025-12-31 16:26:02 +0530

    Time: 88 ms (63.81%) | Memory: 285.4 MB (34.14%) - LeetSync

--- 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.java
+++ 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.java
@@ -1,6 +1,7 @@
 class Solution {
     static List<List<Integer>> list;
     public boolean validPath(int n, int[][] edges, int source, int destination) {
+        // create adjacency list
         list=new ArrayList<>();
         for(int i=0;i<n;i++){
             list.add(new ArrayList<>());
@@ -9,32 +10,34 @@ class Solution {
             list.get(edge[0]).add(edge[1]);
             list.get(edge[1]).add(edge[0]);
         }
+        // do a BFS from source 
         return BFS(n,source,destination);
     }
     public static boolean BFS(int n,int src,int des){
         boolean [] visited=new boolean[n];
         Queue<Integer> q=new LinkedList<>();
-        q.add(src);
+        q.add(src);     // add source node
         while(!q.isEmpty()){
             // remove
             int e=q.poll();
-            // ignore
+            // ignore if already visited
             if(visited[e]){
                 continue;
             }
-            // mark visited
+            // mark the current element visited
             visited[e]=true;
-            // self-work
+            // return true if we have reached the destination
             if(e==des){
                 return true;
             }
-            // add neighbours
+            // add neighbours of the current vertex
             for(int nbr:list.get(e)){
                 if(!visited[nbr]){
                     q.add(nbr);
                 }
             }
         }
+        // we have exited before reaching the destination means it's impossible to reach destination so return false..
         return false;
     }
 }

a583fed1ad5e799d453efa71ee92a319adacf968
Madhav Gupta <madhavguptadavfps1234@gmail.com>
2026-01-03 17:49:22 +0530

    Time: 1 ms (100.00%) | Memory: 48 MB (72.66%) - LeetSync

--- 3189-find-champion-ii/find-champion-ii.java
+++ 3189-find-champion-ii/find-champion-ii.java
@@ -1,9 +1,11 @@
 class Solution {
     public int findChampion(int n, int[][] edges) {
+        // comput the indegree of every node
         int [] inDegree=new int[n];
         for(int [] edge:edges){
             inDegree[edge[1]]++;
         }
+        // count the nodes with indegree 0 and store that node..
         int count=0;
         int node=-1;
         for(int i=0;i<n;i++){
@@ -12,6 +14,7 @@ class Solution {
                 count++;
             }
         }
+        // if there is a single node with indegree 0 means there is only one champion else, there are multiple champions which is not possible so return -1;
         return (count>1)?-1:node;
     }
 }

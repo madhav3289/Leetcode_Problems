d48ef774420d8296d4089201ea932f9328415385
Madhav Gupta <madhavguptadavfps1234@gmail.com>
2026-01-05 16:02:32 +0530

    Time: 0 ms (100.00%) | Memory: 44 MB (41.31%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.java
+++ 74-search-a-2d-matrix/search-a-2d-matrix.java
@@ -2,32 +2,28 @@ class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
         int n=matrix.length;
         int m=matrix[0].length;
+        int [] nums=new int[n*m];
+        int idx=0;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                nums[idx++]=matrix[i][j];
+            }
+        }
+        return BinarySearch(nums,target);
+    }
+    public static boolean BinarySearch(int [] nums,int target){
         int lo=0;
-        int hi=n-1;
+        int hi=nums.length-1;
         while(lo<=hi){
             int mid=lo+(hi-lo)/2;
-            if(matrix[mid][0]<=target && matrix[mid][m-1]>=target){
-                int l=0;
-                int h=m-1;
-                while(l<=h){
-                    int md=l+(h-l)/2;
-                    if(matrix[mid][md]<target){
-                        l=md+1;
-                    }
-                    else if(matrix[mid][md]>target){
-                        h=md-1;
-                    }
-                    else{
-                        return true;
-                    }
-                }
-                return false;
+            if(nums[mid]==target){
+                return true;
             }
-            else if(matrix[mid][0]<target && matrix[mid][m-1]<target){
-                lo=mid+1;
-            }
-            else if(matrix[mid][0]>target && matrix[mid][m-1]>target){
+            else if(nums[mid]>target){
                 hi=mid-1;
+            }
+            else{
+                lo=mid+1;
             }
         }
         return false;

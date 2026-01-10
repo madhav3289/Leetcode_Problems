7a6366eda93fddd9d06e924b3f0413165807bb34
Madhav Gupta <madhavguptadavfps1234@gmail.com>
2026-01-10 20:07:32 +0530

    Time: 34 ms (96.89%) | Memory: 67.4 MB (80.69%) - LeetSync

--- 1087-longest-arithmetic-subsequence/longest-arithmetic-subsequence.java
+++ 1087-longest-arithmetic-subsequence/longest-arithmetic-subsequence.java
@@ -1,36 +1,53 @@
 class Solution {
+    // >>> botton-up (tabulation)
     public int longestArithSeqLength(int[] nums) {
         int n=nums.length;
-        if(n<=2){
-            return 2;
-        }
-        int maxLen=2;
         int [][] dp=new int[n][1002];
-        for(int [] arr:dp){
-            Arrays.fill(arr,-1);
-        }
-        for(int i=0;i<n;i++){
-            for(int j=i+1;j<n;j++){
-                int diff=nums[i]-nums[j];
-                maxLen=Math.max(maxLen,2+helper(nums,diff,i,dp));
+        int maxLen=2;
+        for(int i=1;i<n;i++){
+            for(int j=0;j<i;j++){
+                int diff=nums[i]-nums[j]+501;
+                dp[i][diff]=(dp[j][diff]>0)?dp[j][diff]+1:2;
+                maxLen=Math.max(maxLen,dp[i][diff]);
             }
         }
         return maxLen;
     }
-    public static int helper(int [] nums,int diff,int idx,int [][] dp){
-        if(idx<0){
-            return 0;
-        }
-        if(dp[idx][diff+501]!=-1){
-            return dp[idx][diff+501];
-        }
-        int res=0;
-        for(int i=idx-1;i>=0;i--){
-            if(nums[i]-nums[idx]==diff){
-                int val=1+helper(nums,diff,i,dp);
-                res=Math.max(res,val);
-            }
-        }
-        return dp[idx][diff+501]=res;
-    }
+
+    // >>> top-down (memoization)
+
+    // public int longestArithSeqLength(int[] nums) {
+    //     int n=nums.length;
+    //     if(n<=2){
+    //         return 2;
+    //     }
+    //     int maxLen=2;
+    //     int [][] dp=new int[n][1002];
+    //     for(int [] arr:dp){
+    //         Arrays.fill(arr,-1);
+    //     }
+    //     for(int i=0;i<n;i++){
+    //         for(int j=i+1;j<n;j++){
+    //             int diff=nums[i]-nums[j];
+    //             maxLen=Math.max(maxLen,2+helper(nums,diff,i,dp));
+    //         }
+    //     }
+    //     return maxLen;
+    // }
+    // public static int helper(int [] nums,int diff,int idx,int [][] dp){
+    //     if(idx<0){
+    //         return 0;
+    //     }
+    //     if(dp[idx][diff+501]!=-1){
+    //         return dp[idx][diff+501];
+    //     }
+    //     int res=0;
+    //     for(int i=idx-1;i>=0;i--){
+    //         if(nums[i]-nums[idx]==diff){
+    //             int val=1+helper(nums,diff,i,dp);
+    //             res=Math.max(res,val);
+    //         }
+    //     }
+    //     return dp[idx][diff+501]=res;
+    // }
 }

a94340818bb7c658ef4777a7797e65a2210bd5a2
Madhav Gupta <madhavguptadavfps1234@gmail.com>
2026-01-11 20:32:07 +0530

    Time: 66 ms (18.67%) | Memory: 176.3 MB (8.54%) - LeetSync

--- 2438-find-closest-node-to-given-two-nodes/find-closest-node-to-given-two-nodes.java
+++ 2438-find-closest-node-to-given-two-nodes/find-closest-node-to-given-two-nodes.java
@@ -1,57 +1,45 @@
 class Solution {
-    static List<List<Integer>> list;
+    static List<List<Integer>> graph;
     static int n;
     public int closestMeetingNode(int[] edges, int node1, int node2) {
         n=edges.length;
-        list=new ArrayList<>();
-        // creating adjacency matrix
+        graph=new ArrayList<>();
         for(int i=0;i<n;i++){
-            list.add(new ArrayList<>());
+            graph.add(new ArrayList<>());
         }
         for(int i=0;i<n;i++){
-            // if the value of that edges is -1 means it it not connect to any vertex
             if(edges[i]==-1){
                 continue;
             }
-            list.get(i).add(edges[i]);
+            graph.get(i).add(edges[i]);
         }
-        // pass node1 and compute distace of all other nodes from node1
-        int [] arr1=BFS(node1,n);
-        // pass node2 and compute distace of all other nodes from node2
-        int [] arr2=BFS(node2,n);
-        // resultant index
-        int resIdx=-1;
-        int mini=Integer.MAX_VALUE;
+        int [] n1=BFS(node1);
+        int [] n2=BFS(node2);
+        int minIdx=-1;
+        int minVal=Integer.MAX_VALUE;
         for(int i=0;i<n;i++){
-            // if any of the value is int maximum,
-            // then from either of the two nodes that vertex is unreachable so we continue..
-            if(arr1[i]==Integer.MAX_VALUE || arr2[i]==Integer.MAX_VALUE){
+            if(n1[i]==Integer.MAX_VALUE || n2[i]==Integer.MAX_VALUE){
                 continue;
             }
-            // find the max distance between two
-            int dist=Math.max(arr1[i],arr2[i]);
-            // if the distance is less than mini change the resultant index
-            if(dist<mini){
-                mini=dist;
-                resIdx=i;
+            int val=Math.max(n1[i],n2[i]);
+            if(val<minVal){
+                minVal=val;
+                minIdx=i;
             }
         }
-        return resIdx;
+        return minIdx;
+
     }
-    public static int [] BFS(int node,int n){
+    public static int[] BFS(int node){
         int [] dist=new int[n];
-        // initialise with integer maximum value
         Arrays.fill(dist,Integer.MAX_VALUE);
-        dist[node]=0;   // distance from node to self is 0
+        dist[node]=0;
         Queue<Integer> q=new LinkedList<>();
-        // add the src node
         q.add(node);
         while(!q.isEmpty()){
-            // remove
             int e=q.poll();
-            // add neighbours
-            for(int i:list.get(e)){
-                if(dist[i]>dist[e]+1){
+            for(int i:graph.get(e)){
+                if(dist[e]+1<dist[i]){
                     dist[i]=dist[e]+1;
                     q.add(i);
                 }

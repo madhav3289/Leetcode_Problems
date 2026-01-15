ee11b962276f0ba75cf6049c04517c54aaf361bc
Madhav Gupta <madhavguptadavfps1234@gmail.com>
2026-01-15 23:10:14 +0530

    Time: 73 ms (83.02%) | Memory: 116.8 MB (81.01%) - LeetSync

--- 4130-maximum-score-after-binary-swaps/maximum-score-after-binary-swaps.java
+++ 4130-maximum-score-after-binary-swaps/maximum-score-after-binary-swaps.java
@@ -1,26 +1,12 @@
 class Solution {
     public long maximumScore(int[] nums, String s) {
         int n=nums.length;
-        List<Integer> list=new ArrayList<>();
-        for(int i=0;i<n;i++){
-            if(s.charAt(i)=='1'){
-                list.add(i);
-            }
-        }
-        if(list.size()==0){
-            return 0;
-        }
         long maxScore=0;
-        int index=0;
         PriorityQueue<Integer> pq=new PriorityQueue<>((a,b)->b-a);
         for(int i=0;i<n;i++){
             pq.add(nums[i]);
-            if(i==list.get(index)){
+            if(s.charAt(i)=='1'){
                 maxScore+=pq.poll();
-                index++;
-            }
-            if(index==list.size()){
-                break;
             }
         }
         return maxScore;

bbec8dc5a7831f6707fad2ddc5785e90f1bd05b2
Madhav Gupta <madhavguptadavfps1234@gmail.com>
2026-01-16 23:02:49 +0530

    Time: 826 ms (15.47%) | Memory: 276.8 MB (9.52%) - LeetSync

--- 3250-maximum-square-area-by-removing-fences-from-a-field/maximum-square-area-by-removing-fences-from-a-field.java
+++ 3250-maximum-square-area-by-removing-fences-from-a-field/maximum-square-area-by-removing-fences-from-a-field.java
@@ -1,5 +1,6 @@
 class Solution {
     public int maximizeSquareArea(int m, int n, int[] hFences, int[] vFences) {
+
         // create a list to store columns
         List<Integer> rows=new ArrayList<>();
         for(int i=0;i<hFences.length;i++){
@@ -7,6 +8,7 @@ class Solution {
         }
         rows.add(1);
         rows.add(m);
+
         // create a list to store columns
         List<Integer> cols=new ArrayList<>();
         for(int i=0;i<vFences.length;i++){
@@ -15,7 +17,7 @@ class Solution {
         cols.add(1);
         cols.add(n);
 
-        // store the diff of each pair of rows in rowDiff set
+        // store the diff of each pair of row in rowDiff set
         Set<Integer> rowDif=new HashSet<>();
         for(int i=0;i<rows.size();i++){
             for(int j=i+1;j<rows.size();j++){
@@ -23,7 +25,7 @@ class Solution {
             }
         }
 
-        // store the diff of each pair of cols in colDiff set
+        // store the diff of each pair of col in colDiff set
         Set<Integer> colDif=new HashSet<>();
         for(int i=0;i<cols.size();i++){
             for(int j=i+1;j<cols.size();j++){
@@ -31,6 +33,7 @@ class Solution {
             }
         }
 
+        // calculate the area of largest square
         long maxArea=0;
         for(int num:rowDif){
             if(colDif.contains(num)){
@@ -38,6 +41,7 @@ class Solution {
             }
         }
         int mod=1_000_000_007;
+        // if exists return max%mod else return -1
         return (maxArea==0)?-1:(int)(maxArea%mod);
     }
 }

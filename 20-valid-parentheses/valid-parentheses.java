6356f435f0a521639fb621ac08a5f37b0f114a62
Madhav Gupta <madhavguptadavfps1234@gmail.com>
2026-01-19 14:52:56 +0530

    Time: 3 ms (87.68%) | Memory: 43.3 MB (63.23%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.java
@@ -0,0 +1,28 @@
+class Solution {
+    public boolean isValid(String s) {
+        int n=s.length();
+        Stack<Character> st=new Stack<>();
+        for(int i=0;i<n;i++){
+            char ch=s.charAt(i);
+            if(ch=='(' || ch=='[' || ch=='{'){
+                st.push(ch);
+            }
+            else{
+                if(st.isEmpty()){
+                    return false;
+                }
+                if(ch==')' &&  st.peek()!='('){
+                    return false;
+                }
+                else if(ch==']'  && st.peek()!='['){
+                    return false;
+                }
+                else if(ch=='}'  && st.peek()!='{'){
+                    return false;
+                }
+                st.pop();
+            }
+        }
+        return st.isEmpty();
+    }
+}

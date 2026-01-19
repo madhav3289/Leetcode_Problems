fcc7eb3bb4c16bd845b63ca0ff2ea567ca6a0fe2
Madhav Gupta <madhavguptadavfps1234@gmail.com>
2026-01-20 00:17:32 +0530

    Time: 8 ms (45.69%) | Memory: 54.7 MB (86.29%) - LeetSync

--- 1413-maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold/maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold.java
+++ 1413-maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold/maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold.java
@@ -1,53 +1,23 @@
 class Solution {
-    public int finder(long prefix[][], int i, int j, int len, int threshold)
-    {
-        long sum= prefix[i+len][j+len] - prefix[i][j+len] - prefix[i+len][j] + prefix[i][j];
-        if(sum <= threshold)
-            return len;
-        else
-            return -1;
-    }
-
+    static int n;
+    static int m;
     public int maxSideLength(int[][] mat, int threshold) {
-        int m= mat.length;
-        int n= mat[0].length;
-        long[][] prefix= new long[m+1][n+1];
-
-        for(int i=0; i<m; i++)
-        {
-            for(int j=0; j<n; j++)
-            {
-                prefix[i+1][j+1]= mat[i][j] + prefix[i+1][j]+ prefix[i][j+1] - prefix[i][j];
-            }
-        }
-        
-        int longest= 0;
-        int low= 1, high= Math.min(m,n);
-
-        while(low<=high)
-        {
-            int mid= low+ (high-low)/2;
-            boolean found= false;
-            for(int i=0; i+mid<=m; i++)
-            {
-                for(int j=0; j+mid<=n; j++)
-                {
-                    int length= finder(prefix, i, j, mid, threshold);
-                    if(length!=-1)
-                    {
-                        longest= Math.max(longest, length);
-                        found= true;
-                        break;
-                    }
+        m = mat.length;
+        n = mat[0].length;
+        int res = 0;
+        for(int r=0; r<m; ++r){
+            int tempR = 0;
+            for(int c=0; c<n; ++c){
+                tempR += mat[r][c];
+                mat[r][c] = (r > 0 ? mat[r-1][c] : 0) + tempR;
+                if(r-res >= 0 && c-res>=0 && threshold >= mat[r][c] -
+                (r-res-1 >= 0 ? mat[r-res-1][c] : 0) - 
+                (c-res-1 >= 0 ? mat[r][c-res-1] : 0) + 
+                (c-res-1 >= 0 && r-res-1 >= 0 ? mat[r-res-1][c-res-1] : 0)){
+                    ++res;
                 }
-                if(found)
-                break;
             }
-            if(found)
-                low= mid+1;
-            else
-                high= mid-1;
         }
-        return longest;
+        return res;
     }
 }

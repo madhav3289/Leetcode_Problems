18bd415358f584265965ac6bbd39bd5148a7b6fe
Madhav Gupta <madhavguptadavfps1234@gmail.com>
2026-03-12 15:04:31 +0530

    Time: 59 ms (43.42%) | Memory: 81.9 MB (33.75%) - LeetSync

--- 1447-jump-game-iv/jump-game-iv.java
+++ 1447-jump-game-iv/jump-game-iv.java
@@ -2,50 +2,53 @@ class Solution {
     public int minJumps(int[] arr) {
         int n=arr.length;
         HashMap<Integer,List<Integer>> map=new HashMap<>();
-        for(int i=0;i<arr.length;i++){
-            if(map.containsKey(arr[i])){
-                map.get(arr[i]).add(i);
+        for(int i=0;i<n;i++){
+            int key=arr[i];
+            if(!map.containsKey(key)){
+                map.put(key,new ArrayList<>());
             }
-            else{
-                List<Integer> temp=new ArrayList<>();
-                temp.add(i);
-                map.put(arr[i],temp);
-            }
+            map.get(key).add(i);
         }
+
+        Queue<int[]> q=new LinkedList<>();
+        q.add(new int[]{0,0});
+
+        // to keep a track of visited indices
         boolean [] visited=new boolean[n];
         visited[0]=true;
-        Queue<Integer> q=new LinkedList<>();
-        q.offer(0);
-        int steps=0;
+
         while(!q.isEmpty()){
-            int len=q.size();
-            for(int i=0;i<len;i++){
-                int idx=q.poll();
-                if(idx==n-1){
-                    return steps;
-                }
-                int left=idx-1;
-                int right=idx+1;
-                if(left>=0 && !visited[left]){
-                    visited[left]=true;
-                    q.offer(left);
-                }
-                if(right<n && !visited[right]){
-                    visited[right]=true;
-                    q.offer(right);
-                }
-                if(map.containsKey(arr[idx])){
-                    for(int key:map.get(arr[idx])){
-                        if(!visited[key]){
-                            visited[idx]=true;
-                            q.offer(key);
-                        }
-                    }
-                    map.remove(arr[idx]);
+            // remove
+            int [] p=q.poll();
+            int idx=p[0];
+            int steps=p[1];
+
+            // if reached the last index
+            if(idx==n-1){
+                return steps;
+            }
+
+            // jump 1 step back (i-1)
+            if(idx>0 && !visited[idx-1]){
+                visited[idx-1]=true;
+                q.add(new int[]{idx-1,steps+1});
+            }
+
+            // jump 1 step forward (i+1)
+            if(idx<n-1 && !visited[idx+1]){
+                visited[idx+1]=true;
+                q.add(new int[]{idx+1,steps+1});
+            }
+
+            // jump to indices with same value
+            for(int key:map.get(arr[idx])){
+                if(!visited[key]){
+                    visited[key]=true;
+                    q.add(new int[]{key,steps+1});
                 }
             }
-            steps++;
+            map.get(arr[idx]).clear();
         }
-        return steps;
+        return -1;
     }
 }

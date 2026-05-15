4a32ff51ba7c4908dbc57466e49a8ac7646ad314
Madhav Gupta <madhavguptadavfps1234@gmail.com>
2026-05-15 16:18:18 +0530

    Time: 17 ms (16.39%) | Memory: 73.8 MB (28.25%) - LeetSync

---
+++ 41-first-missing-positive/first-missing-positive.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int firstMissingPositive(int[] nums) {
+        int n=nums.length;
+        Arrays.sort(nums);
+        int val=1;
+        for(int i=0;i<n;i++){
+            if(nums[i]==val){
+                val++;
+            }
+        }
+        return val;
+    }
+}

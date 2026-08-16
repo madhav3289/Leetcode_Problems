ec2739c89c9a8cff63a67922c36c83d8d315cc49
Madhav Gupta <madhavguptadavfps1234@gmail.com>
2026-08-16 22:25:52 +0530

    Time: 0 ms (100.00%) | Memory: 43.5 MB (11.97%) - LeetSync

---
+++ 1248-binary-tree-coloring-game/binary-tree-coloring-game.java
@@ -0,0 +1,61 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+
+    TreeNode xNode;
+
+    public boolean btreeGameWinningMove(TreeNode root, int n, int x) {
+        xNode=null;
+
+        // find the node x
+        findNode(root,x);
+
+        // calculate total number of nodes left subtree and right subtree of x
+
+        // left child
+        int left=calcTotalNodes(xNode.left);
+        // right child
+        int right=calcTotalNodes(xNode.right);
+
+        // parent 
+        int parent=n-(left+right+1);
+
+        int maxi=Math.max(parent,Math.max(left,right));
+        if(maxi>n-maxi){
+            return true;
+        }
+        return false;
+    }
+
+    public int calcTotalNodes(TreeNode root){
+        if(root==null){
+            return 0;
+        }
+        return calcTotalNodes(root.left)+calcTotalNodes(root.right)+1;
+    }
+
+    public void findNode(TreeNode root,int x){
+        if(root==null){
+            return;
+        }
+        if(root.val==x){
+            xNode=root;
+            return;
+        }
+        findNode(root.left,x);
+        findNode(root.right,x);
+    }
+}

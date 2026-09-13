1898deaa1bd3a0f89f55cc7730bab733ff57b2f3
Madhav Gupta <madhavguptadavfps1234@gmail.com>
2026-09-14 00:40:50 +0530

    Time: 7 ms (8.09%) | Memory: 46.4 MB (52.41%) - LeetSync

--- 572-subtree-of-another-tree/subtree-of-another-tree.java
+++ 572-subtree-of-another-tree/subtree-of-another-tree.java
@@ -14,39 +14,123 @@
  * }
  */
 class Solution {
-    public boolean isSubtree(TreeNode root, TreeNode subRoot) {
-        return DFS1(root,subRoot);
-    }
-    public boolean DFS1(TreeNode root,TreeNode subRoot){
-        if(root==null){
-            return false;
-        }
 
-        if(root.val==subRoot.val){
-            if(checkSubTree(root,subRoot)){
-                return true;
+    // Optimal Approach -->> TC : O(n + m)   SC: O(n)
+    // Using KMP algorithm
+
+    public boolean isSubtree(TreeNode root, TreeNode subRoot) {
+        StringBuilder sb1=new StringBuilder();
+        StringBuilder sb2=new StringBuilder();
+
+        serialize(root,sb1);
+        serialize(subRoot,sb2);
+
+        return KMP(sb1.toString(),sb2.toString());
+    }
+    public boolean KMP(String s1,String s2){
+        int n=s1.length();
+        int m=s2.length();
+
+        int [] lps=longestPrefixSuffix(s2,m);
+
+        int i=0;
+        int j=0;
+        while(i<n && j<m){
+            if(s1.charAt(i)==s2.charAt(j)){
+                i++;
+                j++;
+                if(j==m){
+                    return true;
+                }
+            }
+            else{
+                if(j==0){
+                    i++;
+                }
+                else{
+                    j=lps[j-1];
+                }
+            }
+        }
+        return false;
+    }
+    
+    public int[] longestPrefixSuffix(String s,int n){
+        int [] lps=new int[n];
+
+        int i=0;
+        int j=1;
+
+        while(j<n){
+            if(s.charAt(i)==s.charAt(j)){
+                lps[j]=i+1;
+                i++;
+                j++;
+            }
+            else{
+                if(i==0){
+                    lps[j]=0;
+                    j++;
+                }
+                else{
+                    i=lps[i-1];
+                }
             }
         }
 
-        boolean left=DFS1(root.left,subRoot);
-        boolean right=DFS1(root.right,subRoot);
-
-        return left || right;
+        return lps;
     }
-    public boolean checkSubTree(TreeNode root,TreeNode subroot){
-        if(root==null && subroot==null){
-            return true;
-        }
-        if(root==null || subroot==null){
-            return false;
-        }
-        if(root.val!=subroot.val){
-            return false;
+    
+    public void serialize(TreeNode root,StringBuilder sb){
+        if(root==null){
+            sb.append("#,");
+            return;
         }
 
-        boolean left=checkSubTree(root.left,subroot.left);
-        boolean right=checkSubTree(root.right,subroot.right);
+        sb.append(",").append(root.val).append(",");
 
-        return left && right;
+        serialize(root.left,sb);
+        serialize(root.right,sb);
+
+        return;
     }
 }
+
+// Brute Force -->> O(n x m)
+
+
+// public boolean isSubtree(TreeNode root, TreeNode subRoot) {
+//     return DFS1(root,subRoot);
+// }
+// public boolean DFS1(TreeNode root,TreeNode subRoot){
+//     if(root==null){
+//         return false;
+//     }
+
+//     if(root.val==subRoot.val){
+//         if(checkSubTree(root,subRoot)){
+//             return true;
+//         }
+//     }
+
+//     boolean left=DFS1(root.left,subRoot);
+//     boolean right=DFS1(root.right,subRoot);
+
+//     return left || right;
+// }
+// public boolean checkSubTree(TreeNode root,TreeNode subroot){
+//     if(root==null && subroot==null){
+//         return true;
+//     }
+//     if(root==null || subroot==null){
+//         return false;
+//     }
+//     if(root.val!=subroot.val){
+//         return false;
+//     }
+
+//     boolean left=checkSubTree(root.left,subroot.left);
+//     boolean right=checkSubTree(root.right,subroot.right);
+
+//     return left && right;
+// }
